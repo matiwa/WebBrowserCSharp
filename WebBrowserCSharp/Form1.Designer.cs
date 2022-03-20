637792c0daf63a47bab2830dd7104b45f8192a0d
matiwa <mateuszivan1994@wp.pl>
2022-03-20 14:07:03 +0100

    first commit

---
+++ WebBrowserCSharp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WebBrowserCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.cmdBack = new System.Windows.Forms.Button();
+            this.cmdNext = new System.Windows.Forms.Button();
+            this.cmdRefresh = new System.Windows.Forms.Button();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cmdStop = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAdres.Location = new System.Drawing.Point(203, 9);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(591, 20);
+            this.txtAdres.TabIndex = 0;
+            this.txtAdres.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAdres_KeyPress);
+            // 
+            // cmdBack
+            // 
+            this.cmdBack.Enabled = false;
+            this.cmdBack.Location = new System.Drawing.Point(12, 10);
+            this.cmdBack.Name = "cmdBack";
+            this.cmdBack.Size = new System.Drawing.Size(21, 23);
+            this.cmdBack.TabIndex = 1;
+            this.cmdBack.Text = "<";
+            this.cmdBack.UseVisualStyleBackColor = true;
+            this.cmdBack.Click += new System.EventHandler(this.cmdBack_Click);
+            // 
+            // cmdNext
+            // 
+            this.cmdNext.Enabled = false;
+            this.cmdNext.Location = new System.Drawing.Point(39, 10);
+            this.cmdNext.Name = "cmdNext";
+            this.cmdNext.Size = new System.Drawing.Size(21, 23);
+            this.cmdNext.TabIndex = 2;
+            this.cmdNext.Text = ">";
+            this.cmdNext.UseVisualStyleBackColor = true;
+            this.cmdNext.Click += new System.EventHandler(this.cmdNext_Click);
+            // 
+            // cmdRefresh
+            // 
+            this.cmdRefresh.Location = new System.Drawing.Point(66, 10);
+            this.cmdRefresh.Name = "cmdRefresh";
+            this.cmdRefresh.Size = new System.Drawing.Size(52, 23);
+            this.cmdRefresh.TabIndex = 3;
+            this.cmdRefresh.Text = "Refresh";
+            this.cmdRefresh.UseVisualStyleBackColor = true;
+            this.cmdRefresh.Click += new System.EventHandler(this.cmdRefresh_Click);
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Location = new System.Drawing.Point(124, 10);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(30, 23);
+            this.cmdOK.TabIndex = 4;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
+            // 
+            // cmdStop
+            // 
+            this.cmdStop.Location = new System.Drawing.Point(160, 9);
+            this.cmdStop.Name = "cmdStop";
+            this.cmdStop.Size = new System.Drawing.Size(37, 23);
+            this.cmdStop.TabIndex = 5;
+            this.cmdStop.Text = "Stop";
+            this.cmdStop.UseVisualStyleBackColor = true;
+            this.cmdStop.Click += new System.EventHandler(this.cmdStop_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.webBrowser);
+            this.panel1.Location = new System.Drawing.Point(12, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 398);
+            this.panel1.TabIndex = 6;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(776, 398);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cmdStop);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.cmdRefresh);
+            this.Controls.Add(this.cmdNext);
+            this.Controls.Add(this.cmdBack);
+            this.Controls.Add(this.txtAdres);
+            this.Name = "Form1";
+            this.Text = "No page";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Button cmdBack;
+        private System.Windows.Forms.Button cmdNext;
+        private System.Windows.Forms.Button cmdRefresh;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cmdStop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ WebBrowserCSharp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebBrowserCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdOK_Click(object sender, EventArgs e)
+        {
+            if (txtAdres.Text != "") webBrowser.Navigate(txtAdres.Text);
+            else Text = "No page";
+        }
+
+        private void txtAdres_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13 && txtAdres.Text != "") webBrowser.Navigate(txtAdres.Text);
+            else Text = "No page";
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            txtAdres.Text = webBrowser.Url.ToString();
+            Text = webBrowser.DocumentTitle;
+            if (webBrowser.CanGoBack) cmdBack.Enabled = true;
+            else cmdBack.Enabled = false;
+            if (webBrowser.CanGoForward) cmdNext.Enabled = true;
+            else cmdNext.Enabled = false;
+        }
+
+        private void cmdBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void cmdNext_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void cmdRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void cmdStop_Click(object sender, EventArgs e)
+        {
+            webBrowser.Stop();
+        }
+    }
+}
